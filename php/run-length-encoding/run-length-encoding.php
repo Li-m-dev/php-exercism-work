a7e1c85066127ea68168bce5b251fedaf1635893
Li Swafford <liswafford@stacksports.com>
2018-12-13 14:07:51 -0600

    modified code

--- php/run-length-encoding/run-length-encoding.php
+++ php/run-length-encoding/run-length-encoding.php
@@ -1,40 +1,15 @@
 <?php
 
-function encode($input)
-{   
-    if(!$input) {
-        return "";
-    }
-    $count = 0;
-    $output = "";
-    $inputArray = str_split($input);
-    $strCheck = $inputArray[0];
-    foreach($inputArray as $letter) {
-        if ($strCheck == $letter){
-            $count++;
-        } else {
-            if ($count > 1) {
-               $output .=$count;
-            } 
-            $output .= $strCheck;
-            $count = 1;
-        }
-
-        $strCheck = $letter;
-    }
-
-    if($count > 1){
-        $output .=$count;
-    }
-    $output .= $letter;
-
-    return $output;
+function encode ($input)
+{
+    return preg_replace_callback('/(.)\1*/', function($match) {
+        return (strlen($match[0]) === 1 ? $match[1] : strlen($match[0]) . $match[1]);
+    }, $input);
 }
 
 function decode($input)
 {
-    return preg_replace_callback('/([1-9]*)(\w|\s)/', function($match) {
-        return ($match[1] ? str_repeat($match[2], $match[1]) : str_repeat($match[2],1));
+    return preg_replace_callback('/(\d+)(\D)/', function($match) {
+        return str_repeat($match[2], $match[1]);
     }, $input);
 }
- 
